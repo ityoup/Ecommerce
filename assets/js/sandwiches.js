03e45a24fa652e0809c8b382722e0a83d8db5a6b
jclizarraga <jclizarraga@velasresorts.com>
2022-11-13 13:07:05 -0600

    Primer commit

---
+++ assets/js/sandwiches.js
@@ -0,0 +1,10 @@
+const subwayJson = [
+    {id:1, name:'Carbon Vegetal', img:'assets/images/carbon.png', price:[100, 120], sizes:['1kg', '2kg'], description:'Carbon Vegetal'},
+    {id:2, name:'Albasia', img:'assets/images/madera1.png', price:[700, 750], sizes:['1.22 x 2.44m', '2.22 x 3.44m'], description:'Es una madera de replantación bastante abundante en Asia, de crecimiento rápido y un 20 % más leve que el chopo'},
+    {id:3, name:'Caobilla', img:'assets/images/madera2.png', price:[720, 780], sizes:['1.22 x 2.44m', '2.22 x 3.44m'], description:'Carapa guianensis Aubl., es una especie de árboles pertenecientes a la familia de las Meliaceae.'},
+    {id:4, name:'Encino', img:'assets/images/madera3.png', price:[300, 480], sizes:['11.22 x 2.44m', '2.22 x 3.44m'], description:'También conocida como madera de encina, la madera de encino proviene del árbol del mismo nombre, que se caracteriza por su copa ancha.'},
+    {id:5, name:'Parota', img:'assets/images/madera4.png', price:[350, 390], sizes:['1.22 x 2.44m', '2.22 x 3.44m'], description:'La Madera de Parota es reconocida por su increíble belleza dada sus propiedades de color y textura así como su resistencia a la humedad.'},
+    {id:6, name:'Leña', img:'assets/images/madera5.png', price:[50, 90], sizes:['1.22 x 2.44m', '2.22 x 3.44m'], description:'La leña es la madera utilizada para hacer fuego en estufas, chimeneas o cocinas. Es una de las formas más simple de biomasa usada mayormente para calefaccionar y cocinar. Es extraída de árboles y matas.'},
+    {id:7, name:'Kumaru', img:'assets/images/madera6.png', price:[420, 480], sizes:['1.22 x 2.44m', '2.22 x 3.44m'], description:'En nuestro catálogo disponemos de madera tropical de cumarú de gran calidad en formato viga y deck. '},
+    {id:8, name:'Postes', img:'assets/images/madera7.png', price:[60, 80], sizes:['1.22 x 2.44m', '2.22 x 3.44m'], description:'Madera de pino tratada con acabado natural, cepillada y lista para personalizar al gusto o usar como poste.'},
+];
---
+++ assets/js/script.js
@@ -0,0 +1,231 @@
+AOS.init();
+
+// => Animation Loop buyButton
+$(document).ready( () => {
+    let button = document.querySelector('.nav-bar a:last-child');
+    setInterval( () => {        
+        if (button.classList.contains('animation')) {
+            document.querySelector('.nav-bar a:last-child').classList.remove('animation');
+        } else {        
+            button.classList.add('animation');
+        }
+    }, 3500);
+});
+
+// => Scroll to section animation
+const buyButton = document.querySelector('.nav-bar a:last-child');
+
+buyButton.addEventListener('click', scrollOnClick);
+
+function getScrollTopByHref(element) {
+    const id = element.getAttribute("href");
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollOnClick(event) {
+    event.preventDefault();
+    const element = event.target;
+    const to = getScrollTopByHref(event.target) - 80;
+    scrollToPosition(to);
+}
+
+function scrollToPosition(to) {
+    window.scroll({
+        top: to,
+        behavior: "smooth",
+    });
+}
+
+// => Open and Close Menu Mobile
+const toggleMenu = document.querySelector('.toggle');
+toggleMenu.addEventListener('click', function() {
+    let menuMobile = document.querySelector('.header-menu');
+    if (menuMobile.classList.contains('showMenuMobile')) {
+        menuMobile.classList.remove('showMenuMobile');
+        this.classList.add('showMenuMobile');
+    } else {
+        menuMobile.classList.add('showMenuMobile');
+    }
+    document.querySelectorAll('.nav-button').forEach((item) => {
+        item.addEventListener('click', () => {
+            menuMobile.classList.remove('showMenuMobile');
+        });
+    });
+});
+
+// => Carousel
+let currentSlide = 0;
+let totalSlides = document.querySelectorAll('.carousel-item').length;
+document.querySelector('.carousel-inner').style.width = `calc(100vw * ${totalSlides})`;
+document.querySelector('.carousel-dots').style.height = `${document.querySelector('.carousel').clientHeight}px`;
+
+function goPrev() {
+    currentSlide--;
+    if (currentSlide < 0) {currentSlide = totalSlides - 1;}
+    updateMargin();
+}
+function goNext() {
+    currentSlide++;
+    if (currentSlide > (totalSlides - 1)) {currentSlide = 0;}
+    updateMargin();
+}
+
+function updateMargin() {
+    let sliderWidth = document.querySelector('.carousel-item').clientWidth;
+    let newMargin = (currentSlide * sliderWidth);
+    document.querySelector('.carousel-inner').style.marginLeft = `-${newMargin}px`;
+}
+setInterval(goNext, 8000);
+
+// => Sandwiches section
+let cart = [];
+let quant = 1;
+let pizzaKey = 0;
+
+let links = document.querySelectorAll('.nav-button, .footer-button');
+links.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+    });
+});
+
+subwayJson.map( (item, index) => {
+    const subwayItem = document.querySelector('.models .product').cloneNode(true);
+    subwayItem.setAttribute('data-key', index);
+    subwayItem.querySelector('.product--img img').src = item.img;
+    subwayItem.querySelector('.product--price').innerHTML = `${item.price[0].toLocaleString('pt-MX', {style: 'currency', currency: 'MXN'})} (${item.sizes[0]})`;
+    subwayItem.querySelector('.product--name').innerHTML = item.name;
+    subwayItem.querySelector('a').addEventListener('click', (event) => {
+        event.preventDefault();
+        document.querySelector('html').style.overflow = 'hidden';
+        let key = event.target.closest('.product').getAttribute('data-key');
+        quant = 1;
+        pizzaKey = key;        $('input[type=checkbox]').prop('checked',false);
+        document.querySelector('.productImgBig img').src = subwayJson[key].img;
+        document.querySelector('.productInfo h1').innerHTML = subwayJson[key].name;
+        document.querySelector('.productInfo--desc').innerHTML = subwayJson[key].description;
+        document.querySelector('.productInfo--actualPrice').innerHTML = `${subwayJson[key].price[0].toLocaleString('pt-MX', {style: 'currency', currency: 'MXN'})}`;
+        document.querySelector('.productInfo--size.selected').classList.remove('selected');
+        document.querySelectorAll('.productInfo--size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 0) size.classList.add('selected');
+        });
+        document.querySelector('.productInfo--qt').innerHTML = quant;
+        document.querySelector('.modalArea').style.opacity = 0;
+        document.querySelector('.modalArea').style.display = 'flex';
+        setTimeout(() => {            
+            document.querySelector('.modalArea').style.opacity = 1;
+        }, 200);
+    });
+    document.querySelector('.product-area').append( subwayItem );
+});
+
+function closeModal() {
+    document.querySelector('.modalArea').style.opacity = 0;
+    setTimeout( () => {
+        document.querySelector('.modalArea').style.display = 'none';
+    }, 200);
+    document.querySelector('html').style.overflow = 'visible';
+}
+
+document.querySelectorAll('.productInfo--cancelButton, .productInfo--cancelMobileButton').forEach( (item) => {
+    item.addEventListener('click', closeModal);
+});
+
+document.querySelector('.productInfo--qtmenos').addEventListener('click', () => {
+    quant > 1 ? quant -= 1 : quant;
+    document.querySelector('.productInfo--qt').innerHTML = quant;
+});
+
+document.querySelector('.productInfo--qtmais').addEventListener('click', () => {
+    quant++;
+    document.querySelector('.productInfo--qt').innerHTML = quant;
+});
+
+document.querySelectorAll('.productInfo--size').forEach( (size, sizeIndex) => {
+    size.addEventListener('click', () => {
+        document.querySelector('.productInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');        
+        if (sizeIndex === 0){
+            document.querySelector('.productInfo--actualPrice').innerHTML = `${subwayJson[pizzaKey].price[0].toLocaleString('pt-MX', {style: 'currency', currency: 'MXN'})}`;
+        } else {                      
+            document.querySelector('.productInfo--actualPrice').innerHTML = `${subwayJson[pizzaKey].price[1].toLocaleString('pt-MX', {style: 'currency', currency: 'MXN'})}`;
+        }
+    });
+});
+
+document.querySelector('.productInfo--addButton').addEventListener('click', () => {
+    let size = parseInt(document.querySelector('.productInfo--size.selected').getAttribute('data-key'));
+    let identifier = subwayJson[pizzaKey].id + '-' + size;
+    let key = cart.findIndex( (item) => item.identifier == identifier);
+    if ( key > -1 ) {
+        cart[key].qtCart += quant; // => Se já houver um item no carrinho com o mesmo identifier, add a quant.
+    } else {
+        cart.push({
+            identifier,
+            id: subwayJson[pizzaKey].id,
+            size,
+            qtCart: quant,
+            price: subwayJson[pizzaKey].price,
+        });
+    }
+    closeModal();
+    updateCart();
+});
+
+document.querySelector('.cartsection .cart--backBtn').addEventListener('click', () => {
+    document.querySelector('.cartsection').classList.remove('show');
+});
+
+document.querySelector('.cartBtnMobile').addEventListener('click', (event) => {
+    event.preventDefault(); 
+    updateCart();
+});
+
+document.querySelector('.cartBtn').addEventListener('click', (event) => {
+    event.preventDefault(); 
+    updateCart();
+});
+
+function updateCart() {
+    if (cart.length > 0) {
+        document.querySelector('.cartsection').classList.add('show');
+        document.querySelector('.cart').innerHTML = '';
+        document.querySelector('.cartBtn').style.display = 'block';
+        document.querySelector('.cartBtnMobile span').innerHTML = cart.length;
+        document.querySelector('.cartBtn span').innerHTML = cart.length;
+        let total = 0;
+        let totalItem = 0;
+        for (let i in cart) {
+            let subwayItem = subwayJson.find( (item) => item.id == cart[i].id );
+            let cartItem = document.querySelector('.models .cart--item').cloneNode(true);
+            total += subwayItem.price[cart[i].size] * cart[i].qtCart;
+            totalItem += cart[i].price * cart[i].qtCart;
+            cartItem.querySelector('img').src = subwayItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = subwayItem.name;
+            cartItem.querySelector('.cart--item-price').innerHTML = `${subwayItem.price[cart[i].size].toLocaleString('pt-mx',{ style:'currency', currency: 'MXN' })}`;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qtCart;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () => {
+                if (cart[i].qtCart > 1) {
+                    cart[i].qtCart -= 1;
+                } else {
+                    cart.splice(i, 1);
+                }                
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () => {
+                cart[i].qtCart++;
+                updateCart();
+            });
+            document.querySelector('.cart').append( cartItem );
+        }
+        document.querySelector('.total span:last-child').innerHTML = `${total.toLocaleString('pt-mx',{ style:'currency', currency: 'MXN' })}`;
+        document.querySelector('.clearCart p').addEventListener('click', () => {
+            cart = [];
+            updateCart();
+        });
+    } else {
+        document.querySelector('.cartsection').classList.remove('show');
+        document.querySelector('.cartBtnMobile span').innerHTML = cart.length;
+        document.querySelector('.cartBtn span').innerHTML = cart.length;
+    }
+}
